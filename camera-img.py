a3e3c94c5526b7d2d55753d59f521751946f95e3
metasuit <116571620+ccchristoph@users.noreply.github.com>
2022-11-14 14:09:18 +0100

    add camera

---
+++ camera-img.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(2) # Clemens MacBook Pro 16
+# cap = cv2.VideoCapture(?) # Beast Alpha
+
+
+# Check if the webcam is opened correctly
+while not cap.isOpened():
+    print("Camera not found")
+    raise IOError("Error: Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('OMD-D  E-M1', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
